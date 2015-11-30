2d0cc6bcc409740a6111f99c5c7778a331052139
Ben Ebel <bge1234@gmail.com>
2015-11-30 16:48:16 -0700

    finished

--- main.js
+++ main.js
@@ -1,19 +1,54 @@
-var getter = $.ajax({
-  url: "https://www.reddit.com/subreddits/search.json?q=ouya",
-  method: "GET",
-  dataType: "json"
-});
+$("#searchTrigger").click(function() {
+  var searching = $("#searchTerm").val();
+  var address = "https://www.reddit.com/subreddits/search.json?q="
+  address += searching;
+    var getter = $.ajax({
+      url: address,
+      method: "GET",
+      dataType: "json"
+    });
 
-getter.done(function(response) {
-  $("#searchTrigger").click(function() {
-    var searcher = $("#searchTerm").val();
-    for (var i = 0; i < response["data"]["children"].length; i++) {
-      if (response["data"]["children"][i]["data"]["title"].toLowerCase().indexOf(searcher.toLowerCase()) >= 0)
-        $("#tabl").append("<tr><td>" + response["data"]["children"][i]["data"]["title"] + "</td><td><img src=\"" + response["data"]["children"][i]["data"]["header_img"] + "\"></td><td>" + response["data"]["children"][i]["data"]["display_name"] + "</td></tr>");
-    };
+    getter.done(function(response) {
+      var excl = $("#searchExclusion").val();
+
+      for (var i = 0; i < response["data"]["children"].length; i++) {
+        if(excl.length === 0)
+          $("#tabl").append("<tr><td>" + response["data"]["children"][i]["data"]["title"] + "</td><td><img src='" + response["data"]["children"][i]["data"]["header_img"] + "'></td><td>" + response["data"]["children"][i]["data"]["display_name"] + "</td></tr>");
+        else if (response["data"]["children"][i]["data"]["title"].toLowerCase().indexOf(excl.toLowerCase()) === -1)
+          excl = "";
+
+        //Replace non-existent images with placeholders.
+        if(response["data"]["children"][i]["data"]["header_img"] === null) {
+          $("tr:last-child").remove();
+          $("#tabl").append("<tr><td>" + response["data"]["children"][i]["data"]["title"] + "</td><td><img src=\"http:\/\/placehold.it/100x100\"></td><td>" + response["data"]["children"][i]["data"]["display_name"] + "</td></tr>");
+        };
+
+        //Check predicitons against actual and dispay winbox
+        var predicted = parseInt($("#prediction").val());
+        var actual = $('#tabl tr').length;
+        console.log(actual);
+        if (predicted === actual)
+          $("#winbox").append("<table><tr><td style=\"color:white; font-size: 24px; background-color:red;\">You won!</td></tr></table>");
+      };
   });
-});
 
-getter.fail(function(response) {
-  console.log("Failed to get data!");
+  $("#removeTrigger").click(function() {
+    var removal = $("#removeText").val();
+    $("tr:contains(" + removal + ")").html("");
+  });
+
+  $("#findTrigger").click(function() {
+    var target = $("#findText").val();
+    $("tr:contains(" + target + ")").css({"background-color": "yellow"});
+  });
+
+  $("#cleanTrigger").click(function() {
+    $("tr:not(:first-child)").remove();
+    $("#winbox").remove();
+    actual = 0;
+  });
+
+  getter.fail(function(response) {
+    alert("Failed to get data!");
+  });
 });
